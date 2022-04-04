2e13b23e63d2f0fdd9f679cf6992cf11d4567cdb
Biruk <biruk.rcd.1912@gmail.com>
2022-04-04 15:43:51 +0300

    April 4

--- Apr1/app/src/main/java/com/example/apr1/MainActivity.java
+++ Apr1/app/src/main/java/com/example/apr1/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.apr1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentProviderClient;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,13 +19,26 @@ public class MainActivity extends AppCompatActivity {
         b1 = (Button)findViewById(R.id.btn1);
         b2 = (Button)findViewById(R.id.btn2);
         Intent i1 = new Intent(this, MainActivity2.class);
+        String url = "https://google.com/";
 
+
+        Intent i2 = new Intent(Intent.ACTION_VIEW);
         b1.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             startActivity(i1);
         }
     });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent ii2=new Intent(i2.ACTION_VIEW);
+                i2.setData(Uri.parse(url));
+                startActivity(i2);
+            }
+        });
+//        i.setData(Uri.parse(url));
+//        startActivity(i);
 
     }
 }
