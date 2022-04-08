28718887b28cef6dce702749985c809e95ed0bb8
Biruk <biruk.rcd.1912@gmail.com>
2022-04-09 01:39:37 +0300

    april 8

---
+++ April8/app/src/main/java/com/example/april8/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.april8
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+//    Button b1
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Button b1 = (Button)findViewById(R.)
+    }
+}
