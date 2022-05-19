1c45c5fe85ee7e0f28b7f79a7c7cf5bbca825402
Biruk <biruk.rcd.1912@gmail.com>
2022-05-20 00:43:02 +0300

    dialog

---
+++ Dialog/app/src/main/java/com/example/dialog/Dialog.java
@@ -0,0 +1,26 @@
+package com.example.dialog;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class Dialog extends AppCompatDialogFragment {
+    @NonNull
+    @Override
+    public android.app.Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("info")
+                .setMessage("this dialog test")
+                .setNegativeButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+            return builder.create();
+    }
+}
